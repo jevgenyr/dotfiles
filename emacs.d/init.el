94948e8a7094e4ae98e4ff82326ea1c55e8b41ea
Frederic Gingras <frederic@gingras.cc>
2016-03-12 17:30:26 -0500

    Switch to monochome emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -113,7 +113,8 @@
 ; Make tabs 4 wide not 8
 (setq tab-width 4)
 ; Set theme
-(load-theme 'misterioso)
+;(load-theme 'misterioso)
+(load-theme 'quasi-monochrome t)
 
 
 ;;;; keybindings
@@ -263,3 +264,17 @@
 ;;;; init.el end of file
 
 (provide 'my-config)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("436d21e38033b3eec44927e99d0442bc3be5f67889df5f7f674439294256ee6f" "39dd7106e6387e0c45dfce8ed44351078f6acd29a345d8b22e7b8e54ac25bac4" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
